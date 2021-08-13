3d3f23560547c9ebed6f7cff8f93b38e0af23ca0
Anastasia <dianastasiavi@gmail.com>
2021-08-13 18:53:23 +0300

    start project

---
+++ Tasks/Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Anastasia on 13.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
