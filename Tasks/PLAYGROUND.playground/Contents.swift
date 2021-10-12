7dd532c4a3d771dd16614e4a183a7e59190fd11b
Anastasia <dianastasiavi@gmail.com>
2021-10-12 16:23:03 +0300

    little update

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 //B03-01
 //Функція, яка приймає рядок, а повертатиме словник, в якому ключами будуть окремі слова з цього рядка, а значення - кількість їх повторів у реченні
 let stringToChange = "Subscripting a dictionary with a key returns an optional value, because a dictionary might not hold a value for the key that you use in the subscript"
-func createDictionary(from inoutString: String) -> Dictionary<String, Int> {
+func createDictionary(from inoutString: String) -> [String: Int] {
     let words = inoutString.lowercased().components(separatedBy:
                                                             .whitespacesAndNewlines)
     let setOfWords = Array(Set(words).filter{ !$0.hasSuffix(",") })
