38d711af6536e12cc34f64f5d2062c6f472cfe38
Anastasia <dianastasiavi@gmail.com>
2021-10-13 14:27:30 +0300

    upd

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -4,13 +4,12 @@ import UIKit
 let firstArray = [1, 2, 3, 7, 8, 9, 11, 12, 13, 18, 19, 20]
 let secondArray = [3, 4, 5, 6, 7, 8, 10, 11, 13, 20, 1]
 func makeOneArrayFrom(first arrayToCheck1: [Int], second arrayToCheck2: [Int]) -> [Int] {
-    let commonArray = arrayToCheck1 + arrayToCheck2
-    var arrayFromTwo : [Int] = []
-    for sameNumber in commonArray {
-        if arrayToCheck1.contains(sameNumber) && arrayToCheck2.contains(sameNumber) {
-            arrayFromTwo.append(sameNumber)
+    var arrayFromTwoArrays: [Int] = []
+    (arrayToCheck1 + arrayToCheck2).map { num in
+        if arrayToCheck1.contains(num) && arrayToCheck2.contains(num) {
+            arrayFromTwoArrays.append(num)
         }
     }
-    return Array(Set(arrayFromTwo))
+    return Array(Set(arrayFromTwoArrays))
 }
 print (makeOneArrayFrom(first: firstArray, second: secondArray))
