d4e25e51108417b410cf83653a5d63c2e73b5304
Anastasia <dianastasiavi@gmail.com>
2021-10-13 15:39:32 +0300

    in progress

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,4 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
-
+// 1 - Функція має приймати на вхід масив чисел, і повертати суму двох найменших чисел. Виконувати усі необхідні перевіркиКоли задачу зробиш - спробуй впоратись і з додатковим ускладненням (додав підзадачу)
+// 2 - Функція має однаково працювати і з масивом Int і з масивом Float, повертаючи результат того ж типу, який буде на вході.
+let a = [88, 92, 44, 4, 23, 9, 17]
+func toFindSumOfTwoMin(in inoutArray: [Int]) -> Int {
+    let min = inoutArray.min()
+    var nextMin = 0
+    var arr = inoutArray
+    for num in arr {
+        if num == min {
+            arr.remove(at: arr.firstIndex(of: num) ?? 0)
+            nextMin = arr.min() ?? 0
+        }
+    }
+    let sum = min! + nextMin
+    return sum
+}
+print (toFindSumOfTwoMin(in: a))
