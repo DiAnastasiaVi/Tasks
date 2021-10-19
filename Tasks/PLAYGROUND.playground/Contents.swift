317dcf2079d9927b56308f6db292f2c10b1adb7f
Anastasia <dianastasiavi@gmail.com>
2021-10-19 15:32:57 +0300

    update

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -2,18 +2,31 @@ import UIKit
 
 // 1 - Функція має приймати на вхід масив чисел, і повертати суму двох найменших чисел. Виконувати усі необхідні перевіркиКоли задачу зробиш - спробуй впоратись і з додатковим ускладненням (додав підзадачу)
 // 2 - Функція має однаково працювати і з масивом Int і з масивом Float, повертаючи результат того ж типу, який буде на вході.
-let a = [88, 92, 44, 4, 23, 9, 17]
+
+let myNumbers1 = [88, 92, 44, 4, 23, 9, 17]
 func toFindSumOfTwoMin(in inoutArray: [Int]) -> Int {
-    let min = inoutArray.min()
-    var nextMin = 0
-    var arr = inoutArray
-    for num in arr {
-        if num == min {
-            arr.remove(at: arr.firstIndex(of: num) ?? 0)
-            nextMin = arr.min() ?? 0
+    let inoutArrayMin = inoutArray.min()
+var arrayInFunc = inoutArray
+for num in arrayInFunc {
+    if num == inoutArrayMin {
+        arrayInFunc.remove(at: arrayInFunc.firstIndex(of: num) ?? 0)
+    }
+}
+return (inoutArrayMin ?? 0) + (arrayInFunc.min() ?? 0)
+}
+print (toFindSumOfTwoMin(in: myNumbers1))
+
+
+
+let myNumbers2 = [88.11111, 92.2222222, 44.444444444, 4.6666666, 23.6666666, 9.777, 17.999996]
+func toFindSumOfTwoMin(in inoutArray: [Double]) -> Double {
+    let inoutArrayMin = inoutArray.min()
+    var arrayInFunc = inoutArray
+    for num in arrayInFunc {
+        if num == inoutArrayMin {
+            arrayInFunc.remove(at: arrayInFunc.firstIndex(of: num) ?? 0)
         }
     }
-    let sum = min! + nextMin
-    return sum
+    return (inoutArrayMin ?? 0) + (arrayInFunc.min() ?? 0)
 }
-print (toFindSumOfTwoMin(in: a))
+print (toFindSumOfTwoMin(in: myNumbers2))
