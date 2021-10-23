4321e756e00ae332bbc2377fb3d62a19d61a3599
Anastasia <dianastasiavi@gmail.com>
2021-10-23 14:29:47 +0300

    upda

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -4,34 +4,42 @@ import UIKit
 // 2 - Функція має однаково працювати і з масивом Int і з масивом Float, повертаючи результат того ж типу, який буде на вході.
 
 let myNumbers1 = [88, 92, 44, 4, 23, 9, 17, 3]
-func toFindSumOfTwoMin(in inoutArray: [Int]) -> Int {
-    var inoutArrayMin1 = min(inoutArray[0], inoutArray[1])
-    var inoutArrayMin2 = max(inoutArray[0], inoutArray[1])
+let myNumbers2 = [6]
+let myNumbers3 : [Int] = []
+let myNumbers4 = [nil, 15, 12, 7]
+func toFindSumOfTwoMin(in inoutArray: [Int?]) -> Int {
+    guard inoutArray.isEmpty == false else { return 0 }
+    guard inoutArray.count >= 2 else { return inoutArray[0] ?? 0 }
+    guard inoutArray.contains(nil) == false else { return 1 }
+    var inoutArrayMin1 = min(inoutArray[0] ?? 0, inoutArray[1] ?? 0)
+    var inoutArrayMin2 = max(inoutArray[0] ?? 0, inoutArray[1] ?? 0)
     for i in inoutArray {
-        if inoutArrayMin1 > i {
+        if inoutArrayMin1 > i! {
             inoutArrayMin2 = inoutArrayMin1
-            inoutArrayMin1 = i
+            inoutArrayMin1 = i!
         }
         else {
-            inoutArrayMin2 = min(i, inoutArrayMin2)
+            inoutArrayMin2 = min(i!, inoutArrayMin2)
         }
     }
-    return inoutArrayMin1 + inoutArrayMin2
+    return (inoutArrayMin1 ?? 0) + (inoutArrayMin2 ?? 0)
 }
-print (toFindSumOfTwoMin(in: myNumbers1))
-//
-//
-//
-//let myNumbers2 = [88.11111, 92.2222222, 44.444444444, 4.6666666, 23.6666666, 9.777, 17.999996]
-//func toFindSumOfTwoMin(in inoutArray: [Double]) -> Double {
-//    let inoutArrayMin = inoutArray.min()
-//    var arrayInFunc = inoutArray
-//    for num in arrayInFunc {
-//        if num == inoutArrayMin {
-//            arrayInFunc.remove(at: arrayInFunc.firstIndex(of: num) ?? 0)
-//        }
+print(toFindSumOfTwoMin(in: myNumbers1))
+print(toFindSumOfTwoMin(in: myNumbers2))
+print(toFindSumOfTwoMin(in: myNumbers3))
+print(toFindSumOfTwoMin(in: myNumbers4))
+
+
+//var inoutArrayMin1 = min(inoutArray[0], inoutArray[1])
+//var inoutArrayMin2 = max(inoutArray[0], inoutArray[1])
+//for i in inoutArray {
+//    if inoutArrayMin1 > i {
+//        inoutArrayMin2 = inoutArrayMin1
+//        inoutArrayMin1 = i
 //    }
-//    return (inoutArrayMin ?? 0) + (arrayInFunc.min() ?? 0)
+//    else {
+//        inoutArrayMin2 = min(i, inoutArrayMin2)
+//    }
 //}
-//print (toFindSumOfTwoMin(in: myNumbers2))
+//return inoutArrayMin1 + inoutArrayMin2
 
