6d4a2dfe3c318515dd697f183156fea3537b23fa
Anastasia <dianastasiavi@gmail.com>
2021-10-23 17:37:19 +0300

    update

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -10,10 +10,10 @@ let myNumbers4 = [nil, 15, 12, 7]
 func toFindSumOfTwoMin(in inoutArray: [Int?]) -> Int {
     guard inoutArray.isEmpty == false else { return 0 }
     guard inoutArray.count >= 2 else { return inoutArray[0] ?? 0 }
-    guard inoutArray.contains(nil) == false else { return 1 }
-    var inoutArrayMin1 = min(inoutArray[0] ?? 0, inoutArray[1] ?? 0)
-    var inoutArrayMin2 = max(inoutArray[0] ?? 0, inoutArray[1] ?? 0)
-    for i in inoutArray {
+    let nonNilInoutArray = inoutArray.filter{$0 != nil}
+    var inoutArrayMin1 = min(nonNilInoutArray[0]!, nonNilInoutArray[1]!)
+    var inoutArrayMin2 = max(nonNilInoutArray[0]!, nonNilInoutArray[1]!)
+    for i in nonNilInoutArray {
         if inoutArrayMin1 > i! {
             inoutArrayMin2 = inoutArrayMin1
             inoutArrayMin1 = i!
@@ -22,24 +22,9 @@ func toFindSumOfTwoMin(in inoutArray: [Int?]) -> Int {
             inoutArrayMin2 = min(i!, inoutArrayMin2)
         }
     }
-    return (inoutArrayMin1 ?? 0) + (inoutArrayMin2 ?? 0)
+    return inoutArrayMin1 + inoutArrayMin2
 }
 print(toFindSumOfTwoMin(in: myNumbers1))
 print(toFindSumOfTwoMin(in: myNumbers2))
 print(toFindSumOfTwoMin(in: myNumbers3))
 print(toFindSumOfTwoMin(in: myNumbers4))
-
-
-//var inoutArrayMin1 = min(inoutArray[0], inoutArray[1])
-//var inoutArrayMin2 = max(inoutArray[0], inoutArray[1])
-//for i in inoutArray {
-//    if inoutArrayMin1 > i {
-//        inoutArrayMin2 = inoutArrayMin1
-//        inoutArrayMin1 = i
-//    }
-//    else {
-//        inoutArrayMin2 = min(i, inoutArrayMin2)
-//    }
-//}
-//return inoutArrayMin1 + inoutArrayMin2
-
