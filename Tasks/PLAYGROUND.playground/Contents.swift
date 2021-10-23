fab0cb441a198b47500e4043af6c0be632aa0ee2
Anastasia <dianastasiavi@gmail.com>
2021-10-23 20:01:57 +0300

    dictionary yes, array no :(

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -8,7 +8,29 @@ enum ComparingWithN {
 }
 let firstArray = [1, 1, 4, 4, 4, 6, 7, 8, 8, 5, 5, 3, 1, 1]
 func function(inoutArray: [Int], statement: ComparingWithN, N: Int) -> [Int] {
-    
+    var sortedByStatement = inoutArray
+    var dictionaryOfNum : [Int : Int] = [:]
+    for i in inoutArray {
+        dictionaryOfNum[i] = (dictionaryOfNum[i] ?? 0) + 1
+    }
+    for (a, b) in dictionaryOfNum {
+        switch statement {
+        case .theNumberIsRepeatedNTimes:
+            if b == N {
+                dictionaryOfNum.removeValue(forKey: a)
+            }
+        case .theNumberIsRepeatedMoreThanNTimes:
+            if b > N {
+                dictionaryOfNum.removeValue(forKey: a)
+            }
+        case .theNumberIsRepeatedLessThanNTimes:
+            if b < N {
+                dictionaryOfNum.removeValue(forKey: a)
+            }
+        }
+    }
+    print(dictionaryOfNum)
+    return sortedByStatement
 }
 print (function(inoutArray: firstArray, statement: .theNumberIsRepeatedNTimes, N: 3))
 
