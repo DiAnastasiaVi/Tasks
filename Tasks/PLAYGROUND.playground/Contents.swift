4fe1a089ed116a124ea10e360536db3804025978
Anastasia <dianastasiavi@gmail.com>
2021-10-24 15:35:01 +0300

    update 2

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,19 +1,26 @@
 import UIKit
 
-let parenthesisToCheck = "Створити перерахування для дужок ( [ { } ] )"
-let parenthesisToCheck1 = "Уточнення: рахувати саме вкладеність дужок, тобто ) ( i ( { ) } мають повертати false"
+let parenthesisToCheck = "Створити перерахування для дужок"
+let parenthesisToCheck1 = "( [ { } ] )"
+let parenthesisToCheck2 = "Уточнення: рахувати саме вкладеність дужок, тобто ) ( i ( { ) } мають повертати false"
 func checkSpelling(of inoutString: String) -> Bool {
     let parenthesisPairs : [Character : Character] = ["(" : ")", "[" : "]", "{" : "}"]
     var charArray : [Character] = []
     for i in inoutString {
-        if let pairOfParenthesis = parenthesisPairs[i] {
-            charArray.append(pairOfParenthesis)
+        if let pair = parenthesisPairs[i] {
+            charArray.append(pair)
         }
         else if charArray.last == i {
             charArray.popLast()
+        }
+    }
+    for (leftParenthesis, _) in parenthesisPairs {
+        if !inoutString.contains(leftParenthesis) {
+            return false
         }
     }
     return charArray.isEmpty
 }
 print(checkSpelling(of: parenthesisToCheck))
 print(checkSpelling(of: parenthesisToCheck1))
+print(checkSpelling(of: parenthesisToCheck2))
