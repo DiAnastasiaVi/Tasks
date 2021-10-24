d64db985b47d4ac42e3c6e896ce9b1b712115baf
Anastasia <dianastasiavi@gmail.com>
2021-10-24 14:39:50 +0300

    update

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,4 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
-
+let parenthesisToCheck = "Створити перерахування для дужок ( [ { } ] )"
+let parenthesisToCheck1 = "Уточнення: рахувати саме вкладеність дужок, тобто ) ( i ( { ) } мають повертати false"
+func checkSpelling(of inoutString: String) -> Bool {
+    let parenthesisPairs : [Character : Character] = ["(" : ")", "[" : "]", "{" : "}"]
+    var charArray : [Character] = []
+    for i in inoutString {
+        if let pairOfParenthesis = parenthesisPairs[i] {
+            charArray.append(pairOfParenthesis)
+        }
+        else if charArray.last == i {
+            charArray.popLast()
+        }
+    }
+    return charArray.isEmpty
+}
+print(checkSpelling(of: parenthesisToCheck))
+print(checkSpelling(of: parenthesisToCheck1))
