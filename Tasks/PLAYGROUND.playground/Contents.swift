f17639ef917bf5053366b4971675bfce5280989f
Anastasia <dianastasiavi@gmail.com>
2021-10-25 20:39:00 +0300

    find out how to calculate factorials

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,4 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Написати функцію, яка обчислює останні 6 цифр факторіалу числа (якщо воно менше аніж шестизначне - то просто число, якщо більше - то лише останні цифри)
+//n! = (n)*(n-1)*(n-2)*….*2*1
 
+let number = 5
+func factorial(_ n: Int) -> Int {
+    var numberInFunc = 1
+    if n != 0 {
+        numberInFunc = n * factorial(n - 1)
+    }
+    return numberInFunc
+}
+print(factorial(number))
+
+//var numberInFunc = 1
+//if n != 0 {
+//    numberInFunc = n * factorial(n - 1)
+//}
