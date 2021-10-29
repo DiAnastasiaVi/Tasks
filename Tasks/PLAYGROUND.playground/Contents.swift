9cae5b86b4efc1b933a8a4f7584d64336da50eee
Anastasia <dianastasiavi@gmail.com>
2021-10-29 20:06:30 +0300

    upd

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -4,3 +4,20 @@ import UIKit
 //Написати функцію
 //public func solution(_ K : Int, _ A : inout [Int]) -> Int
 //Яка б за даним цілим К та непорожнім масивом А повертала кількість К-комплементарних пар в масиві А.
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+public func solution(_ K : Int, _ A : inout [Int]) -> Int {
+    var P = 0
+    var B = 0
+    var N = A.count
+    
+//    for (firstIndex, firstElement) in A.enumerated() {
+//        for (secondIndex, secondElement) in A.enumerated() {
+//            if firstIndex != secondIndex && firstElement + secondElement == K {
+//
+//            }
+//        }
+//    }
+//    return [1: 0]
+    return K
+}
+print(solution(10, &arr))
