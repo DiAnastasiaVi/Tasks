f46b2a6b53c1ef0ae48f4c359cc57fcc79d5d21f
Anastasia <dianastasiavi@gmail.com>
2021-11-07 19:43:25 +0200

    first try

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,4 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Розробити об’єкти “електроприлади”, до якого належать “принтер” та “сканер”. Обидва мають метод “увімкнути”, але принтер має метод “друкувати”, а сканер - “сканувати”. Створити об’єкт “МФУ” який суміщає властивості принтера та сканера
+struct Printer {
+    let powerSupply = "ON"
+    let itemFunction = "You can print whatever you want to."
+}
+struct Scanner {
+    let powerSupply = "ON"
+    let itemFunction = "You can scan whatever you want to."
+}
+class ElectricalAppliances {
+    var printer = Printer()
+    var scanner = Scanner()
+}
 
+struct New {
+    let powerSupply = ElectricalAppliances().printer.powerSupply
+    let itemFunction = ElectricalAppliances().printer.itemFunction + ElectricalAppliances().scanner.itemFunction
+}
+
+print(New())
