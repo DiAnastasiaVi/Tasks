0517817a8f81b3a7cf9ca1acd4834f71b0a1f30e
Anastasia <dianastasiavi@gmail.com>
2021-12-04 18:22:27 +0200

    weirdo

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,4 +1,31 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Створити групу з 5-ти об’єктів що відповідають символам. У всіх них має бути спільна команда “вивести себе”, за якою вони друкують свій символ. Створити об’єкт, який приймає під час ініціалізації рядок з символів, і за ними формує масив об’єктів.
+//protocol PrintOut {
+//    func printOut(character: Characters)
+//}
+//extension PrintOut {
+//    func printOut(character: Characters) {
+//        print(character.char1)
+//    }
+//}
+protocol PrintOut {
+    func printOut(character: String)
+}
+extension PrintOut {
+    func printOut(character: String) {
+        if character == character {
+            print(character)
+        }
+    }
+}
+struct Characters: PrintOut {
+    let char1 : String = "!"
+    let char2 : String = "@"
+    let char3 : String = "#"
+    let char4 : String = "$"
+    let char5 : String = "%"
+}
 
+let a = Characters()
+a.printOut(character: a.char1)
