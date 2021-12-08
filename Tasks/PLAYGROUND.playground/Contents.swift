6872dfd07dec41ea87f4cc79cf598e9aa0e12c3d
Anastasia <dianastasiavi@gmail.com>
2021-12-08 17:53:51 +0200

    upd2

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,24 +1,24 @@
 import UIKit
 
 //Створити об’єкти “собака”, “мешканець” та “будинок”, створити взаємозв’язки між ними - тобто в будинку проживають мешканці, у кожного мешканця може бути своя собака (собаки). Це значить що кожна собака знає лише свого господаря, а кожен будинок знає лише свої мешканців, і кожен мешканець знає будинок де він проживає та собак які йому належать. Можуть існувати як бездомні люди з собаками так і одинокі собаки. Згенерувати вручну масив собак, жильців та будинків (можна за допомогою генераторів випадкових чисел), і задати яка з собак з яким мешканцем в якому будинку живе. Створити функцію яка буде обходити масив будинків і формувати звіт - які собаки в яких будинках мешкають.
-protocol HouseProtocol {
-    var human: Int { get }
-}
-protocol HumanProtocol {
-    var house: Int { get }
-    var dog: Int { get }
-}
-protocol DogProtocol {
-    var human: Int { get }
-}
-class House: HouseProtocol {
+//protocol HouseProtocol {
+//    var human: Int { get }
+//}
+//protocol HumanProtocol {
+//    var house: Int { get }
+//    var dog: Int { get }
+//}
+//protocol DogProtocol {
+//    var human: Int { get }
+//}
+class House {
     var human = Int.random(in: 0..<10)
 }
-class Human: HumanProtocol {
+class Human {
     var house = Int.random(in: 0..<10)
     var dog = Int.random(in: 0..<10)
 }
-class Dog: DogProtocol {
+class Dog {
     var human = Int.random(in: 0..<10)
 }
 protocol FindDogAndItsHouse {
@@ -29,9 +29,17 @@ extension FindDogAndItsHouse {
         let house = Human().house
         let dog = Human().dog
         let human = House().human
-        let dogWithHouse = "Отчёт: в \(house) дом(ах) проживает \(dog) собак(и)."
+        var dogWithHouse: String = ""
+        if human >= 1 {
+            dogWithHouse = "Отчёт: в \(house) дом(ах) проживает \(dog) собак(и)."
+        }
+        else if dog == 0 {
+            dogWithHouse = "Отчет: в доме не проживает ни одной собаки."
+        }
+        else {
+            dogWithHouse = "Отчет: в доме никто не проживает, собак, увы, нет."
+        }
         print(dogWithHouse)
-        print(house, dog, human)
     }
 }
 class HouseDog: FindDogAndItsHouse {
