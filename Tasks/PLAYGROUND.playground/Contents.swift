e5903533f686bdc9d01816878233964bb54f2948
Anastasia <dianastasiavi@gmail.com>
2021-12-08 15:28:26 +0200

    upd1

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,31 +1,46 @@
 import UIKit
 
 //Створити групу з 5-ти об’єктів що відповідають символам. У всіх них має бути спільна команда “вивести себе”, за якою вони друкують свій символ. Створити об’єкт, який приймає під час ініціалізації рядок з символів, і за ними формує масив об’єктів.
-//protocol PrintOut {
-//    func printOut(character: Characters)
-//}
-//extension PrintOut {
-//    func printOut(character: Characters) {
-//        print(character.char1)
-//    }
-//}
-protocol PrintOut {
-    func printOut(character: String)
+protocol CharacterFunc {
+    var character : String { get }
+    func printOut()
 }
-extension PrintOut {
-    func printOut(character: String) {
+extension CharacterFunc {
+    func printOut() {
         if character == character {
             print(character)
         }
     }
 }
-struct Characters: PrintOut {
-    let char1 : String = "!"
-    let char2 : String = "@"
-    let char3 : String = "#"
-    let char4 : String = "$"
-    let char5 : String = "%"
+struct Char1: CharacterFunc {
+    var character: String = "!"
 }
+struct Char2: CharacterFunc {
+    var character: String = "@"
+}
+struct Char3: CharacterFunc {
+    var character: String = "#"
+}
+struct Char4: CharacterFunc {
+    var character: String = "$"
+}
+struct Char5: CharacterFunc {
+    var character: String = "%"
+}
+protocol CharacterArray {
+    var characterArray: String { get }
+    func makeArray()
+}
+extension CharacterArray {
+    func makeArray() {
+        print(Array(arrayLiteral: characterArray))
+    }
+}
+class Arr: CharacterArray {
+    var characterArray: String = "\(Char1().character), \(Char2().character), \(Char3().character), \(Char4().character), \(Char5().character)"
+}
+let e = Char1()
+e.printOut()
 
-let a = Characters()
-a.printOut(character: a.char1)
+let q = Arr()
+q.makeArray()
