3e3f26d4c2cf52e3868f87019e4f1fd007dc18cf
Anastasia <dianastasiavi@gmail.com>
2021-12-19 11:51:50 +0200

    final update

--- Tasks/PLAYGROUND.playground/Contents.swift
+++ Tasks/PLAYGROUND.playground/Contents.swift
@@ -1,43 +1,57 @@
 import UIKit
 
 //Створити групу з 5-ти об’єктів що відповідають символам. У всіх них має бути спільна команда “вивести себе”, за якою вони друкують свій символ. Створити об’єкт, який приймає під час ініціалізації рядок з символів, і за ними формує масив об’єктів.
-protocol CharacterFunc {
+protocol CharacterProtocol {
     var character : String { get }
     func printOut()
 }
-extension CharacterFunc {
+extension CharacterProtocol {
     func printOut() {
         if character == character {
             print(character)
         }
     }
 }
-struct Char1: CharacterFunc {
+struct Character1: CharacterProtocol {
     var character: String = "!"
 }
-struct Char2: CharacterFunc {
+struct Character2: CharacterProtocol {
     var character: String = "@"
 }
-struct Char3: CharacterFunc {
+struct Character3: CharacterProtocol {
     var character: String = "#"
 }
-struct Char4: CharacterFunc {
+struct Character4: CharacterProtocol {
     var character: String = "$"
 }
-struct Char5: CharacterFunc {
+struct Character5: CharacterProtocol {
     var character: String = "%"
 }
 protocol CharacterArray {
-    func makeArray(stringArray: [String])
+    var characterArray: String { get }
 }
-class Arr: CharacterArray {
-    func makeArray(stringArray: [String]) {
-        var arrayOfChar = [Character]()
-        for i in stringArray {
-            arrayOfChar += i
+extension CharacterArray {
+    func makeArray() -> [CharacterProtocol] {
+        return characterArray.compactMap {
+            switch $0 {
+            case "!":
+                return Character1()
+            case "@":
+                return Character2()
+            case "#":
+                return Character3()
+            case "$":
+                return Character4()
+            case "%":
+                return Character5()
+            default:
+                return nil
+            }
         }
-        print(arrayOfChar)
     }
 }
-let a = Arr()
-a.makeArray(stringArray: [Char1().character, Char2().character, Char3().character, Char4().character, Char5().character])
+class FinalCharacterArray: CharacterArray {
+    var characterArray: String = "\(Character1().character), \(Character2().character), \(Character3().character), \(Character4().character), \(Character5().character)"
+}
+
+
